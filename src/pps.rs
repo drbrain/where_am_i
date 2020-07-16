7d8d042ba29cf404a24e94788c3b59c58afc4507
Eric Hodel <drbrain@segment7.net>
2020-07-15 22:17:42 -0700

    Separate PPS ioctls from usage

--- src/pps.rs
+++ src/pps.rs
@@ -1,101 +1,17 @@
+mod ioctl;
+
 use crate::JsonQueue;
 
 use json::object;
 
-use libc::c_int;
-
 use std::fs::OpenOptions;
-use std::mem;
 use std::os::unix::io::AsRawFd;
 use std::time::SystemTime;
-
-// use nix::sys::ioctl_read;
 
 use tokio::fs::File;
 use tokio::sync::broadcast;
 
-#[derive(Default)]
-#[repr(C)]
-pub struct data {
-    info:    info,
-    timeout: time,
-}
-
-#[derive(Default)]
-#[repr(C)]
-pub struct info {
-    assert_sequence: u32,   // sequence number of assert event
-    clear_sequence:  u32,   // sequence number of clear event
-    assert_tu:       time, // time of assert event
-    clear_tu:        time, // time of clear event
-    current_mode:    i32,   // current mode
-}
-
-#[derive(Default)]
-#[repr(C)]
-pub struct params {
-    api_version:   i32,   // API version
-    mode:          i32,   // current mode
-    assert_off_tu: time, // assert offset compensation
-    clear_off_tu:  time, // clear offset compensation
-}
-
-#[derive(Default)]
-#[repr(C)]
-pub struct time {
-    sec:   i64, // seconds
-    nsec:  i32, // nanoseconds
-    flags: u32, // flags
-}
-
-const TIME_INVALID: u32 = 1<<0;
-
-const CAPTUREASSERT: i32 = 0x01;   // capture assert events
-const CAPTURECLEAR:  i32 = 0x02;   // capture clear events
-const CAPTUREBOTH:   i32 = 0x03;   // capture both event types
-
-const OFFSETASSERT:  i32 = 0x10;   // apply compensation for assert event
-const OFFSETCLEAR:   i32 = 0x20;   // apply compensation for clear event
-
-const ECHOASSERT:    i32 = 0x40;   // feed back assert event to output
-const ECHOCLEAR:     i32 = 0x80;   // feed back clear event to output
-
-const CANWAIT:       i32 = 0x100;  // Can we wait for an event?
-const CANPOLL:       i32 = 0x200;  // Reserved
-
-const DSFMT_TSPEC:   i32 = 0x1000; // struct timespec format
-const DSFMT_NTPFP:   i32 = 0x2000; // NTP time format
-
-const MAGIC: u8 = b'p';
-
-const GETPARAMS: u8 = 0xa1;
-const SETPARAMS: u8 = 0xa2;
-const GETCAP:    u8 = 0xa3;
-const FETCH:     u8 = 0xa4;
-
-// ioctl_read!(getparams, MAGIC, GETPARAMS, params);
-pub unsafe fn getparams(fd: c_int, data: *mut params) -> nix::Result<c_int> {
-    let res = libc::ioctl(fd, request_code_read!(MAGIC, GETPARAMS, mem::size_of::<*mut params>()), data);
-    nix::errno::Errno::result(res)
-}
-// ioctl_write_ptr!(setparams, MAGIC, SETPARAMS, params);
-pub unsafe fn setparams(fd: c_int, data: *mut params) -> nix::Result<c_int> {
-    let res = libc::ioctl(fd, request_code_write!(MAGIC, SETPARAMS, mem::size_of::<*mut params>()), data);
-    nix::errno::Errno::result(res)
-}
-
-ioctl_read!(getcap, MAGIC, GETCAP, i32);
-
-//ioctl_readwrite!(fetch, MAGIC, FETCH, data);
-pub unsafe fn fetch(fd: c_int, data: *mut data) -> nix::Result<c_int> {
-    let res = libc::ioctl(fd, request_code_readwrite!(MAGIC, FETCH, mem::size_of::<*mut data>()), data);
-    nix::errno::Errno::result(res)
-}
-
 pub async fn spawn(device: String) -> JsonQueue {
-    let mode = CAPTUREASSERT;
-    let mut current_mode = 0;
-
     let pps = match OpenOptions::new().read(true).write(true).open(&device) {
         Ok(p) => p,
         Err(e) => {
@@ -107,78 +23,27 @@ pub async fn spawn(device: String) -> JsonQueue {
     eprintln!("Opened {}", device);
     let pps = File::from_std(pps);
 
-    unsafe {
-        let mut params = params::default();
-
-        let result = getparams(pps.as_raw_fd(), &mut params);
-
-        if result.is_err() {
-            let err = result.err().unwrap();
-            match err {
-                nix::Error::Sys(enotty) =>
-                    eprintln!("{} is not a PPS device ({:?})", device, err),
-                _ =>
-                    eprintln!("{} other error ({:?})", device, err),
-            };
-
-            std::process::exit(1);
-        }
-
-        let result = getcap(pps.as_raw_fd(), &mut current_mode);
-
-        if current_mode & CANWAIT == 0 {
-            eprintln!("PPS device {} does not support waiting for pulse", device);
-            std::process::exit(1);
-        }
-
-        if result.is_err() {
-            let err = result.err().unwrap();
-            eprintln!("Unable to get capabilities of {} ({:?})", device, err);
-            std::process::exit(1);
-        }
-
-        if (current_mode & mode) == mode {
-            let result = getparams(pps.as_raw_fd(), &mut params);
-
-            if result.is_err() {
-                let err = result.err().unwrap();
-                eprintln!("Unable to get parameters of {} ({:?})", device, err);
-                std::process::exit(1);
-            }
-
-            params.mode |= mode;
-
-            let result = setparams(pps.as_raw_fd(), &mut params);
-
-            if result.is_err() {
-                let err = result.err().unwrap();
-                eprintln!("Unable to set parameters of {} ({:?})", device, err);
-                std::process::exit(1);
-            }
-        } else {
-            eprintln!("Unable to set mode of {} to {:#06x}, {:#06x}", device, mode, current_mode);
+    match configure(pps.as_raw_fd()) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("{}: {}", device, e);
             std::process::exit(1);
         }
     };
-
-    //if 0 != result {
-    //    eprintln!("{} is not a PPS device ({})", device, result);
-    //    std::process::exit(1);
-    //}
 
     let (tx, _) = broadcast::channel(5);
     let pps_tx = tx.clone();
 
     tokio::spawn(async move {
-        let mut data = data::default();
-        let data_ptr: *mut data = &mut data;
+        let mut data = ioctl::data::default();
+        let data_ptr: *mut ioctl::data = &mut data;
 
         loop {
-            data.timeout.flags = TIME_INVALID;
+            data.timeout.flags = ioctl::TIME_INVALID;
 
             unsafe {
                 let result;
-                result = fetch(pps.as_raw_fd(), data_ptr);
+                result = ioctl::fetch(pps.as_raw_fd(), data_ptr);
 
                 if result.is_err() {
                     let err = result.err().unwrap();
@@ -210,4 +75,47 @@ pub async fn spawn(device: String) -> JsonQueue {
     });
 
     return tx
+}
+
+fn configure(pps_fd: i32) -> Result<bool, String> {
+    unsafe {
+        let mut current_mode = 0;
+        let mut params = ioctl::params::default();
+
+        let result = ioctl::getparams(pps_fd, &mut params);
+
+        if result.is_err() {
+            let err = result.err().unwrap();
+            match err {
+                nix::Error::Sys(enotty) => return Err("not a PPS device".to_string()),
+                e => return Err(format!("other error {:?}", e)),
+            };
+        }
+
+        let result = ioctl::getcap(pps_fd, &mut current_mode);
+
+        if result.is_err() {
+            let err = result.err().unwrap();
+            return Err(format!("unable to get capabilities ({:?})", err));
+        }
+
+        if current_mode & ioctl::CANWAIT == 0 {
+            return Err("cannot wait".to_string());
+        }
+
+        if (current_mode & ioctl::CAPTUREASSERT) == 0 {
+            return Err("cannot capture asserts".to_string());
+        }
+
+        params.mode |= ioctl::CAPTUREASSERT;
+
+        let result = ioctl::setparams(pps_fd, &mut params);
+
+        if result.is_err() {
+            let err = result.err().unwrap();
+            return Err("unable to set parameters".to_string());
+        }
+    }
+
+    Ok(true)
 }
---
+++ src/pps/ioctl.rs
@@ -0,0 +1,83 @@
+use libc::c_int;
+
+use std::mem;
+
+#[derive(Default)]
+#[repr(C)]
+pub struct data {
+    pub info:    info,
+    pub timeout: time,
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct info {
+    pub assert_sequence: u32,   // sequence number of assert event
+    pub clear_sequence:  u32,   // sequence number of clear event
+    pub assert_tu:       time, // time of assert event
+    pub clear_tu:        time, // time of clear event
+    pub current_mode:    i32,   // current mode
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct params {
+    pub api_version:   i32,   // API version
+    pub mode:          i32,   // current mode
+    pub assert_off_tu: time, // assert offset compensation
+    pub clear_off_tu:  time, // clear offset compensation
+}
+
+#[derive(Default)]
+#[repr(C)]
+pub struct time {
+    pub sec:   i64, // seconds
+    pub nsec:  i32, // nanoseconds
+    pub flags: u32, // flags
+}
+
+pub const TIME_INVALID: u32 = 1<<0;
+
+pub const CAPTUREASSERT: i32 = 0x01;   // capture assert events
+pub const CAPTURECLEAR:  i32 = 0x02;   // capture clear events
+pub const CAPTUREBOTH:   i32 = 0x03;   // capture both event types
+
+pub const OFFSETASSERT:  i32 = 0x10;   // apply compensation for assert event
+pub const OFFSETCLEAR:   i32 = 0x20;   // apply compensation for clear event
+
+pub const ECHOASSERT:    i32 = 0x40;   // feed back assert event to output
+pub const ECHOCLEAR:     i32 = 0x80;   // feed back clear event to output
+
+pub const CANWAIT:       i32 = 0x100;  // Can we wait for an event?
+pub const CANPOLL:       i32 = 0x200;  // Reserved
+
+pub const DSFMT_TSPEC:   i32 = 0x1000; // struct timespec format
+pub const DSFMT_NTPFP:   i32 = 0x2000; // NTP time format
+
+pub const MAGIC: u8 = b'p';
+
+pub const GETPARAMS: u8 = 0xa1;
+pub const SETPARAMS: u8 = 0xa2;
+pub const GETCAP:    u8 = 0xa3;
+pub const FETCH:     u8 = 0xa4;
+
+// ioctl_read!(getparams, MAGIC, GETPARAMS, params);
+pub unsafe fn getparams(fd: c_int, data: *mut params) -> nix::Result<c_int> {
+    let res = libc::ioctl(fd, request_code_read!(MAGIC, GETPARAMS, mem::size_of::<*mut params>()), data);
+    nix::errno::Errno::result(res)
+}
+// ioctl_write_ptr!(setparams, MAGIC, SETPARAMS, params);
+pub unsafe fn setparams(fd: c_int, data: *mut params) -> nix::Result<c_int> {
+    let res = libc::ioctl(fd, request_code_write!(MAGIC, SETPARAMS, mem::size_of::<*mut params>()), data);
+    nix::errno::Errno::result(res)
+}
+
+ioctl_read!(getcap, MAGIC, GETCAP, i32);
+
+//ioctl_readwrite!(fetch, MAGIC, FETCH, data);
+pub unsafe fn fetch(fd: c_int, data: *mut data) -> nix::Result<c_int> {
+    let res = libc::ioctl(fd, request_code_readwrite!(MAGIC, FETCH, mem::size_of::<*mut data>()), data);
+    nix::errno::Errno::result(res)
+}
+
+
