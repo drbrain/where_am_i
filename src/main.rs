fb5c833996aee2b8f36c3423fc0dbe987e74375c
Eric Hodel <drbrain@segment7.net>
2020-07-18 11:55:15 -0700

    Try threaded runtime

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod server;
 
 #[macro_use] extern crate nix;
 
+use tokio::runtime;
 use tokio::sync::broadcast;
 
 use tracing::Level;
@@ -15,8 +16,21 @@ use tracing_subscriber;
 pub type JsonReceiver = broadcast::Receiver<json::JsonValue>;
 pub type JsonSender = broadcast::Sender<json::JsonValue>;
 
-#[tokio::main]
-async fn main() {
+fn main() {
+    let mut runtime =
+        runtime::Builder::new()
+        .enable_all()
+        .threaded_scheduler()
+        .on_thread_start(|| { eprintln!("thread started");})
+        .on_thread_stop(|| { eprintln!("thread stopped");})
+        .core_threads(2)
+        .build()
+        .unwrap();
+
+    runtime.block_on(run());
+}
+
+async fn run() {
     let (tx, _) = broadcast::channel(1);
 
     let subscriber = tracing_subscriber::fmt()
@@ -24,7 +38,7 @@ async fn main() {
         .finish();
 
     tracing::subscriber::set_global_default(subscriber)
-      .expect("no global subscriber has been set");
+        .expect("no global subscriber has been set");
 
     let (gps_name, serial_port_settings, pps_name) = args::parse();
 
@@ -39,4 +53,3 @@ async fn main() {
 
     done.await.unwrap();
 }
-
