376b62002ed6395e3be15a7d6c5263be4bdcccc6
Eric Hodel <drbrain@segment7.net>
2020-08-15 00:45:39 -0700

    Send TPV events for the GPSD_JSON NTP driver
    Needed to do a bunch of stuff to get this to work, but it seems to be
    going OK

--- src/gps/test.rs
+++ src/gps/test.rs
@@ -79,10 +79,150 @@ fn test_gga() {
 
     let lat_lon = gps.lat_lon.unwrap();
 
-    assert_eq!(Quality::AutonomousGNSSFix, gps.quality);
+    assert_eq!(Quality::AutonomousGNSSFix, gps.quality.unwrap());
     assert_approx_eq!(44.9343, lat_lon.latitude);
-    assert_approx_eq!(-93.2624, lat_lon.latitude);
+    assert_approx_eq!(-93.2624, lat_lon.longitude);
     assert_approx_eq!(264.0, gps.altitude_msl.unwrap());
+}
+
+#[test]
+fn test_gsa() {
+    let (tx, _) = broadcast::channel(1);
+    let mut gps = GPSData::default();
+
+    let gbgsa = GSAData {
+        talker: Talker::BeiDuo,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::BeiDuo,
+    };
+
+    let gagsa = GSAData {
+        talker: Talker::Galileo,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::Galileo,
+    };
+
+    let glgsa = GSAData {
+        talker: Talker::GLONASS,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::GLONASS,
+    };
+
+    let gpgsa = GSAData {
+        talker: Talker::GPS,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::GPS,
+    };
+
+    gps.gsa(gagsa, "name", &tx);
+    gps.gsa(gbgsa, "name", &tx);
+    gps.gsa(glgsa, "name", &tx);
+    gps.gsa(gpgsa, "name", &tx);
+
+    assert_eq!(None, gps.beiduo_navigation_mode);
+    assert_eq!(None, gps.galileo_navigation_mode);
+    assert_eq!(None, gps.glonass_navigation_mode);
+    assert_eq!(None, gps.gps_navigation_mode);
+}
+
+#[test]
+fn test_gsa_beiduo() {
+    let (tx, _) = broadcast::channel(1);
+    let mut gps = GPSData::default();
+
+    let gbgsa = GSAData {
+        talker: Talker::BeiDuo,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::BeiDuo,
+    };
+
+    gps.gsa(gbgsa, "name", &tx);
+    assert_eq!(NavigationMode::Fix3D, gps.beiduo_navigation_mode.unwrap());
+}
+
+#[test]
+fn test_gsa_galileo() {
+    let (tx, _) = broadcast::channel(1);
+    let mut gps = GPSData::default();
+
+    let gagsa = GSAData {
+        talker: Talker::Galileo,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::Galileo,
+    };
+
+    gps.gsa(gagsa, "name", &tx);
+    assert_eq!(NavigationMode::Fix3D, gps.galileo_navigation_mode.unwrap());
+}
+
+#[test]
+fn test_gsa_glonass() {
+    let (tx, _) = broadcast::channel(1);
+    let mut gps = GPSData::default();
+
+    let glgsa = GSAData {
+        talker: Talker::GLONASS,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::GLONASS,
+    };
+
+    gps.gsa(glgsa, "name", &tx);
+    assert_eq!(NavigationMode::Fix3D, gps.glonass_navigation_mode.unwrap());
+}
+
+#[test]
+fn test_gsa_gps() {
+    let (tx, _) = broadcast::channel(1);
+    let mut gps = GPSData::default();
+
+    let gpgsa = GSAData {
+        talker: Talker::GPS,
+        operation_mode: OperationMode::Automatic,
+        navigation_mode: NavigationMode::Fix3D,
+        satellite_ids: vec![Some(1), Some(2), Some(3)],
+        pdop: 1.0,
+        hdop: 1.0,
+        vdop: 1.0,
+        system: System::GPS,
+    };
+
+    gps.gsa(gpgsa, "name", &tx);
+    assert_eq!(NavigationMode::Fix3D, gps.gps_navigation_mode.unwrap());
 }
 
 #[test]
